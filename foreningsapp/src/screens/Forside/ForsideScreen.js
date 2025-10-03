e52de382790a7383b6b70478a983705e37abe9a3
ostepigen <stine.ettrup@gmail.com>
2025-10-03 13:10:48 +0200

    style

--- foreningsapp/src/screens/Forside/ForsideScreen.js
+++ foreningsapp/src/screens/Forside/ForsideScreen.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import GlobalStyles from "../../styles/globalstyles";
 
 
 export default function ForsideScreen() {
@@ -8,18 +9,32 @@ export default function ForsideScreen() {
         <SafeAreaView>
             <ScrollView>
                 <Text>
-                    <View style={{ flex: 1, padding: 16 }}>
-                        <Text style={{ fontSize: 18, fontWeight: "bold" }}>FORSIDE</Text>
-                        <Text>OPSLAGSTAVLE</Text>
+                    <View style={{ flex: 1, padding: 12 }}>
 
-                        <Text>Vigtige ting fra kalenderen der sker snart
-                            Ting der er vigtige og skal fremhæves (Vand lukkes, arbejdsdag, bestyrelsesmøde)
-                            Evt tilpasset (Har du solgt din bolig, er der ting du skal huske i forskellige faser, har kunne den komme op det det der skulle huskes)
-                            Brugertilpassede funktioner: ligesom man selv kan vælge hvad man ser på sin iphone kunne brugeren også selv tilpasse det. Feks hvis man gerne vil have at den viser hvilket tidspunkt på døgnet der er billigst strøm til at vaske
-                        </Text>
+                        <View style={GlobalStyles.opslagstavle}>
+                            <Text style={GlobalStyles.overskrift}>OPSLAGSTAVLE</Text>
+                           
+                            <View><Text style={GlobalStyles.normaltekst}>Ting fra kalenderen</Text></View>
+                           
+                            <View><Text style={GlobalStyles.normaltekst}>Fremhævede beskeder</Text></View>
+
+                            <View><Text style={GlobalStyles.normaltekst}>Alle opslag</Text></View>
+                        </View>
+
+
+                        <View>
+                            <Text>
+                                Evt tilpasset (Har du solgt din bolig, er der ting du skal huske i forskellige faser, har kunne den komme op det det der skulle huskes)
+                                Brugertilpassede funktioner: ligesom man selv kan vælge hvad man ser på sin iphone kunne brugeren også selv tilpasse det. Feks hvis man gerne vil have at den viser hvilket tidspunkt på døgnet der er billigst strøm til at vaske
+                            </Text>
+                        </View>
+
+
                     </View>
                 </Text>
             </ScrollView>
         </SafeAreaView>
     )
 }
+
+//Forside skærm med opslagstavle, fremhævede ting, brugertilpassede funktioner
--- foreningsapp/src/styles/globalstyles.js
+++ foreningsapp/src/styles/globalstyles.js
@@ -1,1 +1,36 @@
 // fælles style objekter 
+import { StyleSheet } from 'react-native';
+
+const GlobalStyles = StyleSheet.create({
+
+//teskt styles
+    overskrift: {
+        fontSize: 14,
+        fontWeight: "bold",
+                fontFamily: "Helvetica",
+        alignContent: "center",
+        textAlign: "center",
+        padding: 4,
+        marginBottom: 8,
+    },
+    normaltekst: {
+        fontSize: 14,
+        fontFamily: "Times New Roman",
+        padding: 4,
+        marginBottom: 8
+    },
+
+
+// specifikke komponent styles
+  opslagstavle: {
+        backgroundColor: '#ceb89bff',
+        borderRadius: 8,
+        
+       
+    },
+
+
+})
+
+
+export default GlobalStyles;
